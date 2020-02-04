56a81eb088ee3eef6bcefc6f66e27b6618f7bbb8
Fletchlock <47433327+Fletchlock@users.noreply.github.com>
2020-02-04 12:01:21 -0700

    Updates

---
+++ precache-manifest.653dd04f3712fd55ede6a385914b56f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6686b445157d0a9e455c45eb5f1da4fd",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/robo-friends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "c8d04126c141888d0940",
+    "url": "/robo-friends/static/css/main.9c29efe0.chunk.css"
+  },
+  {
+    "revision": "e80c600ad78460bf37e2",
+    "url": "/robo-friends/static/js/2.d6fca685.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robo-friends/static/js/2.d6fca685.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c8d04126c141888d0940",
+    "url": "/robo-friends/static/js/main.b7da6155.chunk.js"
+  },
+  {
+    "revision": "1a8e8ee66596ef9e84f0",
+    "url": "/robo-friends/static/js/runtime-main.74c1282a.js"
+  },
+  {
+    "revision": "31ff09c7ea2e598d131e839bf46a6367",
+    "url": "/robo-friends/static/media/Mismo Campus.31ff09c7.woff"
+  },
+  {
+    "revision": "cad0afe2dc6259372f918e9f0bdc1597",
+    "url": "/robo-friends/static/media/Mismo Regular.cad0afe2.woff"
+  }
+]);
